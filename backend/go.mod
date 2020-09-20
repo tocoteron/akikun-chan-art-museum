c705269cdbd4eb70271c15ddaabcfa6fa5bb16e1
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-21 01:38:45 +0900

    Add twitter client

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,8 @@ module github.com/tokoroten-lab/akikun-chan-art-viewer
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.1.17 // indirect
+require (
+	github.com/dghubble/go-twitter v0.0.0-20200725221434-4bc8ad7ad1b4 // indirect
+	github.com/labstack/echo/v4 v4.1.17 // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 func main() {
+	TwitterAPIKey := os.Getenv("TWITTER_API_KEY")
+	TwitterAPIKeySecret := os.Getenv("TWITTER_API_KEY_SECRET")
+
+	twitterClient := initTwitterClient(TwitterAPIKey, TwitterAPIKeySecret)
+	search, _, err := twitterClient.Search.Tweets(&twitter.SearchTweetParams{
+		Query: "gopher",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(search)
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
@@ -16,6 +35,19 @@ func main() {
 	e.GET("/", hello)
 
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func initTwitterClient(twitterAPIKey, twitterAPIKeySecret string) *twitter.Client {
+	config := &clientcredentials.Config{
+		ClientID:     twitterAPIKey,
+		ClientSecret: twitterAPIKeySecret,
+		TokenURL:     "https://api.twitter.com/oauth2/token",
+	}
+
+	httpClient := config.Client(oauth2.NoContext)
+	twitterClient := twitter.NewClient(httpClient)
+
+	return twitterClient
 }
 
 func hello(c echo.Context) error {
