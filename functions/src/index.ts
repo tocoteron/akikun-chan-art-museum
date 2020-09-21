fe9b36034dd95960395970c4fc745c76da6a446d
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-21 19:19:17 +0900

    Enable CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import * as cors from 'cors';
 import {
   initTwitterClient,
   aysncSearchTweets,
@@ -6,6 +7,8 @@ import {
   TweetAuthor,
   TweetImage
 } from './twitter';
+
+const corsHandler = cors({ origin: true });
 
 const twitterClient = initTwitterClient(
   functions.config().twitter.apikey,
@@ -20,48 +23,55 @@ export const helloWorld = functions.region("asia-northeast1").https.onRequest((r
   response.send("Hello from Firebase 2!");
 });
 
+async function getTweets(): Promise<Tweet[]> {
+  const searchRes = await aysncSearchTweets(twitterClient, {
+    tweet_mode: "extended",
+    q: "#アキくんちゃんアート exclude:retweets",
+    count: 100,
+  });
+
+  const tweets: Tweet[] = searchRes.statuses
+    .filter((tweet: any) => {
+      return tweet.extended_entities && tweet.extended_entities.media;
+    })
+    .map((tweet: any) => {
+      const author: TweetAuthor = {
+        id: tweet.user.id_str,
+        name: tweet.user.name,
+        screenName: tweet.user.screen_name,
+      }
+
+      const images: TweetImage[] = tweet.extended_entities.media.map((image: any) => ({
+        url: image.media_url_https,
+        width: image.sizes.large.w,
+        height: image.sizes.large.h,
+      }));
+
+      return {
+        id: tweet.id_str,
+        url: `https://twitter.com/${author.screenName}/status/${tweet.id_str}`,
+        text: tweet.full_text,
+        author,
+        images,
+        createdAt: new Date(tweet.created_at),
+      };
+    });
+
+  return tweets;
+}
+
 export const akikunChanArts = functions.region("asia-northeast1").https.onRequest(async (req, res) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  functions.logger.info("/akikunChanArts", {structuredData: true});
 
-  if (req.method === "GET") {
+  corsHandler(req, res, async () => {
     try {
-      const searchRes = await aysncSearchTweets(twitterClient, {
-        tweet_mode: "extended",
-        q: "#アキくんちゃんアート exclude:retweets",
-        count: 100,
-      });
-
-      const tweets: Tweet[] = searchRes.statuses
-        .filter((tweet: any) => {
-          return tweet.extended_entities && tweet.extended_entities.media;
-        })
-        .map((tweet: any) => {
-          const author: TweetAuthor = {
-            id: tweet.user.id_str,
-            name: tweet.user.name,
-            screenName: tweet.user.screen_name,
-          }
-
-          const images: TweetImage[] = tweet.extended_entities.media.map((image: any) => ({
-            url: image.media_url_https,
-            width: image.sizes.large.w,
-            height: image.sizes.large.h,
-          }));
-
-          return {
-            id: tweet.id_str,
-            url: `https://twitter.com/${author.screenName}/status/${tweet.id_str}`,
-            text: tweet.full_text,
-            author,
-            images,
-            createdAt: new Date(tweet.created_at),
-          };
-        });
-
-      res.status(200).json(tweets);
+      if (req.method === "GET") {
+        const tweets = await getTweets();
+        res.status(200).json(tweets);
+      }
     } catch (err) {
       functions.logger.error(err, {structuredData: true});
       res.sendStatus(500);
     }
-  }
+  });
 });
