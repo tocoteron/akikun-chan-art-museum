d8012f7f4e3c7269e5280bae9945de4d7bf5d8e8
tokoroten-lab <tokoroten.lab@gmail.com>
2020-09-21 18:22:52 +0900

    Add akikunChanArts endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,67 @@
 import * as functions from 'firebase-functions';
+import {
+  initTwitterClient,
+  aysncSearchTweets,
+  Tweet,
+  TweetAuthor,
+  TweetImage
+} from './twitter';
+
+const twitterClient = initTwitterClient(
+  functions.config().twitter.apikey,
+  functions.config().twitter.apikeysecret,
+  functions.config().twitter.bearertoken
+);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
 export const helloWorld = functions.region("asia-northeast1").https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase 2!");
+});
+
+export const akikunChanArts = functions.region("asia-northeast1").https.onRequest(async (req, res) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+
+  if (req.method === "GET") {
+    try {
+      const searchRes = await aysncSearchTweets(twitterClient, {
+        tweet_mode: "extended",
+        q: "#アキくんちゃんアート exclude:retweets",
+        count: 100,
+      });
+
+      const tweets: Tweet[] = searchRes.statuses
+        .filter((tweet: any) => {
+          return tweet.extended_entities && tweet.extended_entities.media;
+        })
+        .map((tweet: any) => {
+          const creator: TweetAuthor = {
+            id: tweet.user.id_str,
+            name: tweet.user.name,
+            screenName: tweet.user.screen_name,
+          }
+
+          const images: TweetImage[] = tweet.extended_entities.media.map((image: any) => ({
+            url: image.media_url_https,
+            width: image.sizes.large.w,
+            height: image.sizes.large.h,
+          }));
+
+          return {
+            id: tweet.id_str,
+            url: `https://twitter.com/${creator.screenName}/status/${tweet.id_str}`,
+            text: tweet.full_text,
+            creator,
+            images,
+            createdAt: new Date(tweet.created_at),
+          };
+        });
+
+      res.status(200).json(tweets);
+    } catch (err) {
+      functions.logger.error(err, {structuredData: true});
+      res.sendStatus(500);
+    }
+  }
 });
---
+++ functions/src/twitter.ts
@@ -0,0 +1,41 @@
+import * as Twitter from 'twitter';
+
+export interface TweetAuthor {
+  id: string;
+  name: string;
+  screenName: string;
+}
+
+export interface TweetImage {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Tweet {
+  id: string;
+  url: string;
+  text: string;
+  creator: TweetAuthor;
+  images: TweetImage[];
+  createdAt: Date;
+}
+
+export function initTwitterClient(apiKey: string, apiKeySecret: string, bearerToken: string): Twitter {
+  return new Twitter({
+    consumer_key: apiKey,
+    consumer_secret: apiKeySecret,
+    bearer_token: bearerToken,
+  });
+}
+
+export function aysncSearchTweets(twitterClient: Twitter, searchOption: Twitter.RequestParams): Promise<Twitter.ResponseData> {
+  return new Promise((resolve, reject) => {
+    twitterClient.get('search/tweets', searchOption, function(error, tweets) {
+      if (error) {
+        reject(error);
+      }
+      resolve(tweets);
+    });
+  });
+}
